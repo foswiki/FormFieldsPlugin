f7e4d4fada1fd0321df96f4766c62be177e94f20
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/FormFieldsPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/FormFieldsPlugin.pm
+++ lib/TWiki/Plugins/FormFieldsPlugin.pm
@@ -12,7 +12,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -29,24 +29,23 @@
 #   insidePREHandler     ( $text )
 #   endRenderingHandler  ( $text )
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name.
-# 
+#
 # NOTE: To interact with TWiki use the official TWiki functions
 # in the &TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
-
 
 # =========================
 package TWiki::Plugins::FormFieldsPlugin;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $debug
-        $exampleCfgVar $writeScript %template
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $debug
+  $exampleCfgVar $writeScript %template
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -58,184 +57,194 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between FormFieldsPlugin and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning(
+            "Version mismatch between FormFieldsPlugin and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "FORMFIELDSPLUGIN_DEBUG" );
-    
+    $debug = &TWiki::Func::getPreferencesFlag("FORMFIELDSPLUGIN_DEBUG");
+
     $writeScript = "";
-    %template = ();
+    %template    = ();
 
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::FormFieldsPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    &TWiki::Func::writeDebug(
+        "- TWiki::Plugins::FormFieldsPlugin::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
 # =========================
-sub init
-{
-   # Lazy initiation
-   if( ! $template{"SCRIPT"} ) {
-       my $text = &TWiki::Func::readTemplate("formfields");
-       my @sections = split( /%SPLIT%/o, $text );
-       foreach my $section ( @sections ) {
-           if( $section =~ /^([^:]+):/o ) {
-               my $key= $1;
-               $section =~ s/^([^:]+)://o;
-               $template{$1} = $section;
-           }
-       }
-   }
+sub init {
+
+    # Lazy initiation
+    if ( !$template{"SCRIPT"} ) {
+        my $text = &TWiki::Func::readTemplate("formfields");
+        my @sections = split( /%SPLIT%/o, $text );
+        foreach my $section (@sections) {
+            if ( $section =~ /^([^:]+):/o ) {
+                my $key = $1;
+                $section =~ s/^([^:]+)://o;
+                $template{$1} = $section;
+            }
+        }
+    }
 }
 
-sub multiselectItem
-{
-   my( $value ) = @_;
-   my $item = $template{"MULTI-ITEM"};
-   $item =~ s/%VALUE%/$value/g;
-   return $item;
+sub multiselectItem {
+    my ($value) = @_;
+    my $item = $template{"MULTI-ITEM"};
+    $item =~ s/%VALUE%/$value/g;
+    return $item;
 }
 
-sub singleselectItem
-{
-   my( $value, $current ) = @_;
-   my $item = $template{"SINGLE-ITEM"};
-   $item =~ s/%VALUE%/$value/go;
-   my $selected = ($value eq $current) ? "selected=\"selected\"" : "";
-   $item =~ s/%SELECTED%/$selected/go;
-   return $item;
+sub singleselectItem {
+    my ( $value, $current ) = @_;
+    my $item = $template{"SINGLE-ITEM"};
+    $item =~ s/%VALUE%/$value/go;
+    my $selected = ( $value eq $current ) ? "selected=\"selected\"" : "";
+    $item =~ s/%SELECTED%/$selected/go;
+    return $item;
 }
 
-sub colourChooser
-{
-   my( $name, $current, $possibleValues ) = @_;
-   
-   $current = $possibleValues if( ! $current );
-   my $html = $template{"COLOUR-CHOOSER"};
-   $html =~ s/%NAME%/$name/g;
-   $html =~ s/%VALUE%/$current/g;
-   return $html;
+sub colourChooser {
+    my ( $name, $current, $possibleValues ) = @_;
+
+    $current = $possibleValues if ( !$current );
+    my $html = $template{"COLOUR-CHOOSER"};
+    $html =~ s/%NAME%/$name/g;
+    $html =~ s/%VALUE%/$current/g;
+    return $html;
 }
 
+sub multiselect {
+    my ( $name, $size, $current, $possibleValues ) = @_;
 
-sub multiselect
-{
-   my( $name, $size, $current, $possibleValues ) = @_;
-   
-   my $right = "<select multiplex=\"true\" size=\"10\" name=\"${name}R\" style=\"width:150\">\n"; 
-   my @currentValues = split( /,/, $current );
-   my %selected = ();
-   my $sep = "";
-   my $values = "";
-   foreach my $item ( @currentValues ) {
-      $selected{$item} = 1;
-      $right .= multiselectItem($item);
-      $values .= "$sep$item";
-      $sep = ", ";
-   }
-   $right .= "</select>\n";
-   
-   $left = $template{"MULTI-START-LEFT"};
-   $left =~ s/%NAME%/$name/go;
-   foreach my $item ( @${possibleValues} ) {
-       $left .= "   <option value=\"$item\">$item</option>\n" if( ! $selected{$item} );
-   }
-   $left .= "</select>\n";
-   
-   my $ret = $template{"MULTI"};
-   $ret =~ s/%LEFT%/$left/go;
-   $ret =~ s/%NAME%/$name/go;
-   $ret =~ s/%RIGHT%/$right/go;
-   $ret =~ s/%VALUE%/$values/go;
-   
-   return $ret;
+    my $right =
+"<select multiplex=\"true\" size=\"10\" name=\"${name}R\" style=\"width:150\">\n";
+    my @currentValues = split( /,/, $current );
+    my %selected      = ();
+    my $sep           = "";
+    my $values        = "";
+    foreach my $item (@currentValues) {
+        $selected{$item} = 1;
+        $right  .= multiselectItem($item);
+        $values .= "$sep$item";
+        $sep = ", ";
+    }
+    $right .= "</select>\n";
+
+    $left = $template{"MULTI-START-LEFT"};
+    $left =~ s/%NAME%/$name/go;
+    foreach my $item ( @${possibleValues} ) {
+        $left .= "   <option value=\"$item\">$item</option>\n"
+          if ( !$selected{$item} );
+    }
+    $left .= "</select>\n";
+
+    my $ret = $template{"MULTI"};
+    $ret =~ s/%LEFT%/$left/go;
+    $ret =~ s/%NAME%/$name/go;
+    $ret =~ s/%RIGHT%/$right/go;
+    $ret =~ s/%VALUE%/$values/go;
+
+    return $ret;
 }
 
-sub singleselect
-{
-   my( $name, $size, $current, $possibleValues, $fix ) = @_;
-   
-   my $select = "";
-   #if( $fix eq "no" ) {
-   #    $select .= singleselectItem("--free form--", $current ? "--free form--" : $current);
-   #}
-   foreach my $item ( @$possibleValues ) {
-       $select .= singleselectItem($item, $current);
-   }
-   
-   my $ret = $template{"SINGLE"};
-   
-   $ret =~ s/%NAME%/$name/go;
-   $ret =~ s/%SIZE%/$size/go;
-   $ret =~ s/%VALUE%/$current/go;
-   $ret =~ s/%OPTIONS%/$select/o;
-   
-   return $ret;
+sub singleselect {
+    my ( $name, $size, $current, $possibleValues, $fix ) = @_;
+
+    my $select = "";
+
+#if( $fix eq "no" ) {
+#    $select .= singleselectItem("--free form--", $current ? "--free form--" : $current);
+#}
+    foreach my $item (@$possibleValues) {
+        $select .= singleselectItem( $item, $current );
+    }
+
+    my $ret = $template{"SINGLE"};
+
+    $ret =~ s/%NAME%/$name/go;
+    $ret =~ s/%SIZE%/$size/go;
+    $ret =~ s/%VALUE%/$current/go;
+    $ret =~ s/%OPTIONS%/$select/o;
+
+    return $ret;
 }
 
 # =========================
-sub renderFormFieldForEditHandler
-{
+sub renderFormFieldForEditHandler {
     my ( $name, $type, $size, $value, $attributes, $possibleValues ) = @_;
 
-    &TWiki::Func::writeDebug( "- FormFieldsPlugin::renderFormFieldForEditHandler( $web.$topic )" ) if $debug;
+    &TWiki::Func::writeDebug(
+        "- FormFieldsPlugin::renderFormFieldForEditHandler( $web.$topic )")
+      if $debug;
 
-    # This handler is called by Form.renderForEdit, before built in types are considered
+# This handler is called by Form.renderForEdit, before built in types are considered
 
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::FormFieldsPlugin::renderFormFieldForEditHandler( $_[0], $_[1] ) call" ) if $debug;
-    
+    &TWiki::Func::writeDebug(
+"- TWiki::Plugins::FormFieldsPlugin::renderFormFieldForEditHandler( $_[0], $_[1] ) call"
+    ) if $debug;
+
     my $ret = "";
+
     #my $name = $_[0];
     #my $type = $_[1];
     #if( $type eq "text" ) {
     #    $ret = "xxx";
     #}
-    
+
     ## TODO: currently uses TWiki::addScript, if this looks good then should move to Functions.pm
-    
+
     my $needScript = "";
-    
-    if( $type eq "multiselect" ) {
+
+    if ( $type eq "multiselect" ) {
+
         # FIXME remove this option
         init();
         $ret = &multiselect( $name, $size, $value, $possibleValues );
-        &TWiki::addScript("toShow[toShow.length]=\"${name}s\";\ntoHide[toHide.length]=\"${name}ns\";");
+        &TWiki::addScript(
+"toShow[toShow.length]=\"${name}s\";\ntoHide[toHide.length]=\"${name}ns\";"
+        );
         $needScript = 1;
-    } elsif( $type =~ /colou?rchooser/o ) {
+    }
+    elsif ( $type =~ /colou?rchooser/o ) {
         init();
         $ret = &colourChooser( $name, $value, $possibleValues );
         $needScript = 1;
-    } elsif( $type =~ /select *(.*)/o ) {
+    }
+    elsif ( $type =~ /select *(.*)/o ) {
         my $args = $1;
         my $fix = &TWiki::Func::extractNameValuePair( $args, "fix" ) || "";
-        if( $fix eq "no" ) {
-        init();
-        $ret = &singleselect( $name, $size, $value, $possibleValues, $fix );
+        if ( $fix eq "no" ) {
+            init();
+            $ret = &singleselect( $name, $size, $value, $possibleValues, $fix );
             &TWiki::addScript("toHide[toHide.length]=\"${name}\";");
             $needScript = 1;
-        } elsif( $multi eq "twopane" ) {
+        }
+        elsif ( $multi eq "twopane" ) {
             init();
             $ret = &multiselect( $name, $size, $value, $possibleValues );
-            &TWiki::addScript("toShow[toShow.length]=\"${name}s\";\ntoHide[toHide.length]=\"${name}ns\";");
+            &TWiki::addScript(
+"toShow[toShow.length]=\"${name}s\";\ntoHide[toHide.length]=\"${name}ns\";"
+            );
             $needScript = 1;
         }
     }
-    
-    if( $needScript && ! $writeScript ) {
+
+    if ( $needScript && !$writeScript ) {
         $writeScript = 1;
         &TWiki::addScript( $template{"SCRIPT"} );
     }
-    
+
     return $ret;
 }
 
